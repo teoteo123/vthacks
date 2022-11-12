39321ed62ee34fa66e1c7dca689daf9fafab8da1
Alex Danilov <dai@vt.edu>
2022-11-12 15:56:07 -0500

    Add approval system

--- forge/src/RentingContract.sol
+++ forge/src/RentingContract.sol
@@ -14,6 +14,13 @@ contract RentingContract {
         // rentDates is a bitmap that stores two years worth of rent dates
         // each bit represents a day, 0 is not rented, 1 is rented
         uint256[3] rentedDates;
+        Application[] pendingApplications;
+    }
+
+    struct Application {
+        address applicant;
+        uint256 start;
+        uint256 end;
     }
 
     Listing[] listings;
@@ -59,11 +66,11 @@ contract RentingContract {
         _;
     }
 
-    modifier listingNotRented(
-        uint256 _id,
-        uint256 _start,
-        uint256 _end
-    ) {
+    modifier listingNotRented(uint256 _id, uint256 _applicationId) {
+        uint256 _start = listings[_id]
+            .pendingApplications[_applicationId]
+            .start;
+        uint256 _end = listings[_id].pendingApplications[_applicationId].end;
         require(_start < _end, "Start date must be before end date");
         require(_end - _start <= 365 * 2, "Cannot rent for more than 2 years");
         for (uint256 i = _start; i < _end; i++) {
@@ -83,34 +90,93 @@ contract RentingContract {
         _;
     }
 
-    event LogListingRented(
+    event LogApplicationSent(uint256 listingId, uint256 start, uint256 end);
+
+    function applyForListing(
+        uint256 _id,
+        uint256 _start,
+        uint256 _end
+    ) public listingExists(_id) notOwnerApplying(_id) {
+        listings[_id].pendingApplications.push(
+            Application(msg.sender, _start, _end)
+        );
+
+        emit LogApplicationSent(_id, _start, _end);
+    }
+
+    modifier ownerApproving(uint256 _id) {
+        require(
+            listings[_id].owner == msg.sender,
+            "Only owner can approve an application"
+        );
+        _;
+    }
+
+    event LogApplicationApproved(
         address renter,
         uint32 id,
         uint256 start,
         uint256 end
     );
 
-    function applyForListing(
-        uint32 _id,
-        uint64 _start,
-        uint64 _end
-    )
+    function approveApplication(uint32 _id, uint32 _applicationId)
         public
         listingExists(_id)
-        listingNotRented(_id, _start, _end)
-        notOwnerApplying(_id)
+        listingNotRented(_id, _applicationId)
+        ownerApproving(_id)
     {
-        while (_start <= _end) {
-            listings[_id].rentedDates[_start / 256] |= 1 << (_start % 256);
-            _start++;
+        uint256 _start = listings[_id]
+            .pendingApplications[_applicationId]
+            .start;
+        uint256 _end = listings[_id].pendingApplications[_applicationId].end;
+        address _renter = listings[_id]
+            .pendingApplications[_applicationId]
+            .applicant;
+        for (uint256 day = _start; day <= _end; day++) {
+            listings[_id].rentedDates[day / 256] |= 1 << (day % 256);
         }
-        listings[_id].renters.push(msg.sender);
+        listings[_id].renters.push(_renter);
         listings[_id].rentDates.push([_start, _end]);
 
-        emit LogListingRented(msg.sender, _id, _start, _end);
+        emit LogApplicationApproved(_renter, _id, _start, _end);
+
+        delete listings[_id].pendingApplications[_applicationId];
+
+        // look for all applications that cannot be approved anymore and delete them
+        uint256 numApplications = listings[_id].pendingApplications.length;
+        for (uint256 i = 0; i < numApplications; i++) {
+            uint256 _currentStart = listings[_id].pendingApplications[i].start;
+            uint256 _currentEnd = listings[_id].pendingApplications[i].end;
+            for (uint256 day = _currentStart; day <= _currentEnd; day++) {
+                if (_start <= day && day <= _end) {
+                    delete listings[_id].pendingApplications[i];
+                    i--;
+                    numApplications--;
+                    break;
+                }
+            }
+        }
     }
 
     function getListings() public view returns (Listing[] memory) {
         return listings;
+    }
+
+    function getListingById(uint256 _id)
+        public
+        view
+        listingExists(_id)
+        returns (Listing memory)
+    {
+        return listings[_id];
+    }
+
+    function getApplicationsForListing(uint256 _id)
+        public
+        view
+        listingExists(_id)
+        returns (Application[] memory)
+    {
+        return listings[_id].pendingApplications;
     }
 }
